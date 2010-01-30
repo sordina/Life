7e5da78d959d89b345c48d75f14010f7b59deaae
Lyndon Maydwell <maydwell@gmail.com>
2010-01-31 06:18:43 +0800

    Added keyboard shortcuts

--- Life.hs
+++ Life.hs
@@ -1,12 +1,3 @@
-{--
-    http://en.wikipedia.org/wiki/Conway's_Game_of_Life
-
-    Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
-    Any live cell with more than three live neighbours dies, as if by overcrowding.
-    Any live cell with two or three live neighbours lives on to the next generation.
-    Any dead cell with exactly three live neighbours becomes a live cell.
---}
-
 -- External imports
 import Graphics.UI.GLUT
 import Data.IORef
@@ -30,14 +21,14 @@ main = do
 
   mainLoop
 
-window title width height display = do
+window title width height displayCB = do
   createWindow title
   windowSize $= Size width height
-  displayCallback $= display
-  keyboardMouseCallback $= Just km
+  displayCallback $= displayCB
+  keyboardMouseCallback $= Just (mkKM displayCB)
 
   translate (Vector3 (0-1) (0-1) (0::GLfloat))
-  scale 0.04 0.04 (0::GLfloat)
+  scale 0.04 0.04 (0::GLfloat) -- Should tie this to the board size
 
 -- Pops off the head of the life-list and renders it at second intervals.
 display :: IORef [LifeSnapshot] -> IORef POSIXTime -> IO ()
@@ -45,7 +36,9 @@ display lifeList timeIO = do
   previousTime <- get timeIO
   currentTime <- getPOSIXTime
 
-  when (currentTime > (previousTime + 1)) $ do
+  -- leave this until we get propper rendering going
+  -- when (currentTime > (previousTime + 1)) $ do
+  do
     print currentTime
     timeIO $= (previousTime + 1)
 
@@ -58,11 +51,9 @@ display lifeList timeIO = do
     flush
     swapBuffers
 
-km :: Key -> KeyState -> c -> d -> IO ()
-km (Char 'f') Down _ _ = fullscreen
-km _ _ _ _ = return ()
-
-fullscreen :: IO ()
-fullscreen = do
-  print "Entering fullscreen mode."
-  fullScreenToggle
+mkKM displayCB = km
+  where
+    km :: Key -> KeyState -> c -> d -> IO ()
+    km (Char 'n') Down _ _ = displayCB
+    km (Char 'q') Down _ _ = exit
+    km _ _ _ _ = return ()
--- LifeMatrix.hs
+++ LifeMatrix.hs
@@ -1,3 +1,12 @@
+{--
+    http://en.wikipedia.org/wiki/Conway's_Game_of_Life
+
+    Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+    Any live cell with more than three live neighbours dies, as if by overcrowding.
+    Any live cell with two or three live neighbours lives on to the next generation.
+    Any dead cell with exactly three live neighbours becomes a live cell.
+--}
+
 module LifeMatrix (randomGame, LifeSnapshot, Health (Alive, Dead), toListWithPos) where
 
 -- Libraries

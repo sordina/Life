c45f26768e71376a28747bd87e5e4b45a7dac34e
Lyndon Maydwell <maydwell@gmail.com>
2010-01-31 06:04:21 +0800

    Life logic seems sound, still no constant rendering though...

--- Life.hs
+++ Life.hs
@@ -22,11 +22,11 @@ main = do
   _ <- getArgsAndInitialize
   initialDisplayMode $= [DoubleBuffered]
 
-  lifeList <- randomGame 10
+  lifeList <- randomGame 50
   lifeListIO <- newIORef lifeList
   timeIO <- newIORef =<< getPOSIXTime
 
-  window "LIFE" 100 100 (display lifeListIO timeIO)
+  window "LIFE" 200 200 (display lifeListIO timeIO)
 
   mainLoop
 
@@ -34,6 +34,10 @@ window title width height display = do
   createWindow title
   windowSize $= Size width height
   displayCallback $= display
+  keyboardMouseCallback $= Just km
+
+  translate (Vector3 (0-1) (0-1) (0::GLfloat))
+  scale 0.04 0.04 (0::GLfloat)
 
 -- Pops off the head of the life-list and renders it at second intervals.
 display :: IORef [LifeSnapshot] -> IORef POSIXTime -> IO ()
@@ -47,8 +51,18 @@ display lifeList timeIO = do
 
     clear [ColorBuffer]
     snap:snaps <- get lifeList
-    putStrLn $ show snap
+    --putStrLn $ show snap
     lifeList $= snaps
     renderSnapshot snap
+    --renderBounds
     flush
     swapBuffers
+
+km :: Key -> KeyState -> c -> d -> IO ()
+km (Char 'f') Down _ _ = fullscreen
+km _ _ _ _ = return ()
+
+fullscreen :: IO ()
+fullscreen = do
+  print "Entering fullscreen mode."
+  fullScreenToggle

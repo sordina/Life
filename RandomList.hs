bd74cabfe606ccfb6ffb449cece95e2c1e1c674a
Lyndon Maydwell <maydwell@gmail.com>
2010-01-30 19:14:44 +0800

    Updating interface to have new methods

--- RandomList.hs
+++ RandomList.hs
@@ -1,6 +1,7 @@
-module RandomList where
+module RandomList (randomListValue, randomList) where
 
 import Random
+import ListUtils
 
 randomListValue :: RandomGen g => g -> [a] -> (g,a)
 randomListValue gen list = (g,val)
@@ -9,9 +10,28 @@ randomListValue gen list = (g,val)
     (i,g) = randomR (0,l) gen 
     l = length list
 
-randomList :: (RandomGen g, Integral i) => g -> [a] -> i -> [a]
-randomList gen list len = map snd $ take (fromIntegral len) l
+randomList :: RandomGen g => g -> [a] -> [a]
+randomList gen list = map snd l
   where
     l = iterate f i
     i = randomListValue gen list
     f (g, val) = randomListValue g list
+
+randomListN :: (RandomGen g, Integral i) => g -> [a] -> i -> [a]
+randomListN gen list len = take (fromIntegral len) (randomList gen list)
+
+randomRows :: (RandomGen g, Integral i) => g -> [a] -> i -> [[a]]
+randomRows gen list width = splitLen (fromIntegral width) l
+  where
+    l = randomList gen list
+
+randomRowsN :: (RandomGen g, Integral i) => g -> [a] -> i -> i -> [[a]]
+randomRowsN gen list width len = take (fromIntegral len) (randomRows gen list (fromIntegral width))
+
+{-- Can't test StdGen elements for some ungodly reason.
+
+prop_elem :: StdGen -> [Integer] -> Bool
+prop_elem gen list = result `elem` list
+  where (g, result) = randomListValue gen list
+
+-}
